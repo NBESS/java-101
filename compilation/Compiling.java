d6347d3e00d9217fb6bba6136263b433f0ddf708
NBESS <nicholasdbess@gmail.com>
2020-10-30 14:47:29 -0400

    Add example comment lines

--- compilation/Compiling.java
+++ compilation/Compiling.java
@@ -1,6 +1,6 @@
 public class Compiling {
     public static void main(String[] args) {
-        
+        // This is how you write single line comments 
         System.out.println("Java is a class-based language.");
 
         System.out.println("Java classes have a 'main' method");
@@ -8,5 +8,9 @@ public class Compiling {
         System.out.println("Java statements end with a semicolon");
 
         System.out.println("Programming is ... fun!");
+        /* 
+        This is an example of multi-line comment.
+        Additional commented line
+        */
     }
 }
