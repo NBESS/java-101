5bd1ac06d8a4fab46aa34f559ba92811751fff58
NBESS <nicholasdbess@gmail.com>
2020-10-30 15:25:47 -0400

    Update Tree.java

---
+++ plant-a-tree/Tree.java
@@ -0,0 +1,19 @@
+public class Tree {
+    public static void main(String[] args) {
+        /* This program will contain an introduction
+            and print a tree in the console
+        */
+        System.out.println("Hey there, I'm Nick and I'm having a blast learning to code Java!");
+        System.out.println("I'm going to plant a tree today!");
+
+        System.out.println("Ready to get my hands dirty!");
+
+        System.out.println(" ***** ");
+        System.out.println(" ***** ");
+        System.out.println(" ***** ");
+        System.out.println("   *    ");
+        System.out.println("   *  "); 
+        System.out.println("#######"); 
+
+    }
+}
