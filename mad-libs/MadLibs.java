371425f0e9734271f763c76e051e0286975ee09a
NBESS <nicholasdbess@gmail.com>
2020-11-01 14:38:44 -0500

    Create MadLibs.java
    Exercise
    Java Variables: Mad Libs

---
+++ mad-libs/MadLibs.java
@@ -0,0 +1,35 @@
+public class MadLibs {
+  /*
+  This program generates a mad libbed story.
+  Author: Nick B.
+  Date: Nov. 01, 2020
+  */
+  	public static void main(String[] args){
+      
+      
+      
+        // The template for the story
+        String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+        // Name
+        String name1 = "Thomas";
+        String name2 = "Sheryl";
+        // Adjectives
+        String adjective1 = "tall"; 
+        String adjective2 = "bright";
+        String adjective3 = "fantastic";
+        // Verbs
+        String verb1 = "threw";
+        // Nouns
+        String noun1 = "frog";
+        String noun2 = "singer";
+        String noun3 = "toad";
+        String noun4 = "tiger";
+        String noun5 = "spike";
+        String noun6 = "gaming console";
+        // Number
+        int number = 7;
+        // Place
+        String place1 = "Detroit";
+
+    }       
+}
