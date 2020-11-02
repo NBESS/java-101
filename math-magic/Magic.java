17ffba8a13da4420d835d53995b49ee9323ff7f3
NBESS <nicholasdbess@gmail.com>
2020-11-02 15:08:20 -0500

    complete steps 2 through 9

--- math-magic/Magic.java
+++ math-magic/Magic.java
@@ -1,8 +1,22 @@
 public class Magic {
 	public static void main(String[] args) {
-    
-    int MyNumber = 5;
-		
+    // myNumber is the original number
+    int myNumber = 5;
+    // set equal to the original number multiplied by itself
+	int stepOne = myNumber * myNumber;
+    // Set it equal to the previous result (stepOne) plus the original number
+    int stepTwo = stepOne + myNumber;
+    // Set equal to the previous result (stepTwo) divided by the original number
+    int stepThree = stepTwo / myNumber;
+    // Set equal to the previous result (stepThree) plus 17
+    int stepFour = stepThree + 17;
+    // Set equal to the previous result (stepFour) minus the original number
+    int stepFive = stepFour - myNumber;
+    // Set equal to the previous result (stepFive) divided by 6
+    int stepSix = stepFive / 6;
+    // Print out the value of the last step
+    System.out.println(stepSix);
+
 
 	}
 }
