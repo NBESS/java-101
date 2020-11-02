aca16fdcbc6c7471e8ded4478af68d0f0ac4f4e3
NBESS <nicholasdbess@gmail.com>
2020-11-02 12:36:07 -0500

    Complete MadLibs

--- mad-libs/MadLibs.java
+++ mad-libs/MadLibs.java
@@ -4,12 +4,7 @@ public class MadLibs {
   Author: Nick B.
   Date: Nov. 01, 2020
   */
-  	public static void main(String[] args){
-      
-      
-      
-        // The template for the story
-        String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+  	public static void main(String[] args){   
         // Name
         String name1 = "Thomas";
         String name2 = "Sheryl";
@@ -30,6 +25,11 @@ public class MadLibs {
         int number = 7;
         // Place
         String place1 = "Detroit";
+        // The template for the story
+        String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+        
+        // Print the story.
+        System.out.println(story);
 
     }       
 }
