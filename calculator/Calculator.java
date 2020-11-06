40844ebdd0b248d9d346bac9a84109a9dcbc6451
NBESS <nicholasdbess@gmail.com>
2020-11-06 15:32:55 -0500

    Add Calculator program

---
+++ calculator/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+    // Instance field
+
+    // Constructor method
+    public Calculator() {
+    }
+
+    // Add method
+    public int add(int numOne, int numTwo) {
+        int result = numOne + numTwo;
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
