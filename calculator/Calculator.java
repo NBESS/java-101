3522d60eebcaf15c2836733d0f0113a5e520df9d
NBESS <nicholasdbess@gmail.com>
2020-11-06 17:54:47 -0500

    Add multiply, divide, and modulo methods

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -5,6 +5,11 @@ public class Calculator {
     public Calculator() {
     }
 
+    // toString method
+    public String toString() {
+        return "The calculator has various methods that can be used to perform mathematical operations"
+    }
+
     // Add method
     public int add(int numOne, int numTwo) {
         int result = numOne + numTwo;
@@ -12,7 +17,7 @@ public class Calculator {
         System.out.println(numOne + " + " + numTwo + " = " + result);
         return result;
     }
-
+ 
     // Subtract method
     public int subtract(int numOne, int numTwo) {
         int result = numOne - numTwo;
@@ -21,11 +26,38 @@ public class Calculator {
         return result;
     }
 
+    // Multiply method
+    public int multiply(int numOne, int numTwo) {
+        int result = numOne * numTwo;
+
+        System.out.println(numOne + " * " + numTwo + " = " + result);
+        return result;
+    }
+
+    // Divide method
+    public int divide(int numOne, int numTwo) {
+        int result = numOne / numTwo;
+
+        System.out.println(numOne + " / " + numTwo + " = " + result);
+        return result;
+    }
+
+    // Modulo method
+    public int modulo(int numOne, int numTwo) {
+        int result = numOne % numTwo;
+
+        System.out.println(numOne + " % " + numTwo + " = " + result);
+        return result;
+    }
+
     public static void main(String[] args) {
         Calculator myCalculator = new Calculator();
 
-        System.out.println(myCalculator.add(5, 7));
-        System.out.println(myCalculator.subtract(12, 7));
+        myCalculator.add(5, 7);
+        myCalculator.subtract(12, 7);
+        myCalculator.multiply(100, 4);
+        myCalculator.divide(100, 4);
+        myCalculator.modulo(100, 2);
 
     }
 }
