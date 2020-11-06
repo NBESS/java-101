f77a38f4e96b90c3778b34630c2d913cfa2ae7be
NBESS <nicholasdbess@gmail.com>
2020-11-06 16:24:44 -0500

    Add methods for addition and subtraction

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -8,11 +8,24 @@ public class Calculator {
     // Add method
     public int add(int numOne, int numTwo) {
         int result = numOne + numTwo;
+
+        System.out.println(numOne + " + " + numTwo + " = " + result);
         return result;
     }
 
+    // Subtract method
+    public int subtract(int numOne, int numTwo) {
+        int result = numOne - numTwo;
+
+        System.out.println(numOne + " - " + numTwo + " = " + result);
+        return result;
+    }
 
     public static void main(String[] args) {
+        Calculator myCalculator = new Calculator();
+
+        System.out.println(myCalculator.add(5, 7));
+        System.out.println(myCalculator.subtract(12, 7));
 
     }
 }
