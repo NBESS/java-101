d1c4118de0447c4dae9a8bdd8706e4339a3316eb
NBESS <nicholasdbess@gmail.com>
2020-11-17 08:15:03 -0500

    Fix syntax errors

--- car-loan-calculator/CarLoan.java
+++ car-loan-calculator/CarLoan.java
@@ -1,7 +1,15 @@
-public class CarLoan() {
+public class CarLoan {
+    public static void main(String[] args) {   
+    int carLoan = 10000;
+    int loanLength = 3;
+    int interestRate = 5;
+    int downPayment = 2000;
+
+    if (loanLength <= 0 || interestRate <= 0) {
+        System.out.println("Error! You must take out a valid car loan.");
+    }
 
 
-    public static void main(String[] args) {
-        
+
     }
 }
