526b24d89dd2f4ce4428570c66cd127e3deabdd0
NBESS <nicholasdbess@gmail.com>
2020-11-17 10:00:00 -0500

    Complete CarLoan program; Add Continents

--- car-loan-calculator/CarLoan.java
+++ car-loan-calculator/CarLoan.java
@@ -1,5 +1,10 @@
 public class CarLoan {
-    public static void main(String[] args) {   
+    public static void main(String[] args) {  
+
+    /* 
+    This program combines conditionals, Boolean expressions, and arithmetic expressions to calculate the monthly car payment a user should expect to make after taking out a car loan. 
+    */
+
     int carLoan = 10000;
     int loanLength = 3;
     int interestRate = 5;
@@ -7,9 +12,17 @@ public class CarLoan {
 
     if (loanLength <= 0 || interestRate <= 0) {
         System.out.println("Error! You must take out a valid car loan.");
+    } else if (downPayment > carLoan) {
+        System.out.println("You have enough funds to pay the car in full.");
+    } else {
+        int remainingBalance = carLoan - downPayment;
+        int months = loanLength * 12;
+        int monthlyBalance = remainingBalance / months;
+        int interest = (monthlyBalance * interestRate) / 100; 
+        int monthlyPayment = monthlyBalance + interest;
+        
+        System.out.println(monthlyPayment);
     }
-
-
 
     }
 }
