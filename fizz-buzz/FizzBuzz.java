18cae940fa4c12f1d25f8334f37817ace5c8eed9
NBESS <nicholasdbess@gmail.com>
2020-11-23 22:35:07 -0500

    Complete FizzBuzz

--- fizz-buzz/FizzBuzz.java
+++ fizz-buzz/FizzBuzz.java
@@ -2,10 +2,20 @@ public class FizzBuzz {
 
     public static void main(String[] args) {
         for (int i = 1; i<= 100; i++) {
-            
-            System.out.println(i);
-        }
-
+            // if number is divisible by 3 and 5 print FizzBuzz
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            // if number is divisible by 5 print Buzz
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            // if number is divisible by 3 print Fizz
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            // print number
+            } else {
+                System.out.println(i);
+            }
+        }   
     }
 
 }
