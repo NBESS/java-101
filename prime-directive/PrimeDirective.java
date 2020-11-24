c5ab8dd270127f872a34657e63b381d3c1279ea3
NBESS <nicholasdbess@gmail.com>
2020-11-24 11:00:18 -0500

    Create PrimeDirective

---
+++ prime-directive/PrimeDirective.java
@@ -0,0 +1,12 @@
+// Import statement: 
+import java.util.ArrayList;
+
+class PrimeDirective {
+
+    public static void main(String[] args) {
+
+        PrimeDirective pd = new PrimeDirective();
+        int[] numbers = {6, 29, 28, 33, 11, 100, 101, 43, 89};
+
+    }
+}
